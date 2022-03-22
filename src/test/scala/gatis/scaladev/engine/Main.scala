7e0c4f4eae258f63c0bb1983c2e6608c827386a5
Gatis Meikulis <gmeikulis@evolution.com>
2022-03-22 13:02:06 +0200

    add testing library and run first test

---
+++ src/test/scala/gatis/scaladev/engine/Main.scala
@@ -0,0 +1,12 @@
+package gatis.scaladev.engine
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class Main extends AnyFunSuite{
+
+  test("simple testing should work") {
+    val output = true
+    assert(output)
+  }
+
+}
